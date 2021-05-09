4c36fb66ae4d52b8d5339adefbc7f48ad78835f7
Rosales Bryan <18030602@itcelaya.edu.mx>
2021-05-09 14:48:14 -0500

    Agregadas test de mokito en la clase EcuacionPrimerGradoMockitoTest

--- src/main/java/EcuacionPrimerGradoMockitoTest.java
+++ src/main/java/EcuacionPrimerGradoMockitoTest.java
@@ -10,5 +10,44 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 public class EcuacionPrimerGradoMockitoTest {
+    @InjectMocks
+    private EcuacionPrimerGrado ecuacionPrimerGrado;
 
+    @Mock
+    private Parseador parseador;
+
+    @Before
+    public void inicializaMocks() {
+        MockitoAnnotations.initMocks(this);
+}@Test
+    public void solucionaEcuacionConMenos() {
+        String ecuacion = "2x - 1 = 0";
+
+        when(parseador.obtenerParte1(ecuacion)).thenReturn(2);
+        when(parseador.obtenerParte2(ecuacion)).thenReturn(-1);
+        when(parseador.obtenerParte3(ecuacion)).thenReturn(0);
+
+        Double result = ecuacionPrimerGrado.obtenerResultado(ecuacion);
+
+        Double valueExpected = 0.5;
+
+        assertEquals(valueExpected, result);
+    }
+
+    @Test
+    public void solucionaEcuacionConMas() {
+
+        String ecuacion = "2x + 1 = 0";
+
+        when(parseador.obtenerParte1(ecuacion)).thenReturn(2);
+        when(parseador.obtenerParte2(ecuacion)).thenReturn(1);
+        when(parseador.obtenerParte3(ecuacion)).thenReturn(0);
+
+        Double result = ecuacionPrimerGrado.obtenerResultado(ecuacion);
+
+        Double valueExpected = -0.5;
+
+        assertEquals(valueExpected, result);
+    }
 }
+
